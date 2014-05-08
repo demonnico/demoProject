f24a3a565e3ab24d4cd196fc645162adee35b423
Nicholas Tau <fengping.tao@upai.com>
2014-05-08 10:10:28 +0800

    set key window

--- demoProject/NTAppDelegate.m
+++ demoProject/NTAppDelegate.m
@@ -7,12 +7,16 @@
 //
 
 #import "NTAppDelegate.h"
+#import "NTViewController.h"
 
 @implementation NTAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.rootViewController = [NTViewController new];
+    [self.window makeKeyAndVisible];
     return YES;
 }
 							
